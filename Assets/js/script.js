3a165a544bae28bdb206d2c0ffffd6ebb7cf3743
cathytanya <cathycolette.tanya@gmail.com>
2021-10-01 10:07:55 -0400

    add chart to cryptoside

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -68,6 +68,36 @@ let clearButton = document.querySelector('#clearBtn')
       source: bitcoin
     });
 } );
+// demo chart
+let chart = document.getElementById("cyptochart");
+let lineCrypto = new Chart(chart,{
+  type: "line",
+  data:{
+      label:["January","February","March","April","May","June","July"],
+      datasets: [{
+        label: 'My First Dataset',
+        fill: false,
+        tension: 0.1,
+        backgroundColor: "red",
+        borderColor: 'red',
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJointStyle: "miter",
+        pointBorderColor: "purple",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "orange",
+        pointHoverBorderColor: "green",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,        
+        data: ["65", "59", "80", 81, 56, 55, 40],
+      }
+    ]
+} });
+
 
 function clearEntries() {
     document.getElementById('tags').value = ''
